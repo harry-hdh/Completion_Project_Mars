e8bdf3d993c7413af1cc8daaf4a740d51f1c1e06
harry-hdh <hdh997@outlook.com>
2024-11-25 21:20:00 +1300

    Added Certifications model

---
+++ Test_Project_Mars_Selenium/Utilities/CertificationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project_Mars_Selenium.Utilities
+{
+    public class CertificationModel
+    {
+        public string? CertName { get; set; }
+
+        public string? From { get; set; }
+
+        public string? AchieveYear { get; set; }
+    }
+}
