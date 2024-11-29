45f9fccfa128eef85340c98998d62ac941f5ff29
harry-hdh <hdh997@outlook.com>
2024-11-29 17:23:16 +1300

    Added invalid tests for both education and certification pages

--- Test_Project_Mars_Selenium/Tests/ProfileCertifications.cs
+++ Test_Project_Mars_Selenium/Tests/ProfileCertifications.cs
@@ -15,7 +15,7 @@ namespace Test_Project_Mars_Selenium.Tests
 
         [Test]
         [TestCaseSource(nameof(AddCert))]
-        public void AddProfileCerti(CertificationModel certificationModel)
+        public void AddCertificationTest(CertificationModel certificationModel)
         {
             PerformEducationAction(
                 action: () => profileCertObj.AddCert(_driver,
@@ -30,7 +30,7 @@ namespace Test_Project_Mars_Selenium.Tests
 
         [Test]
         [TestCaseSource(nameof(EditCert))]
-        public void EditProfileCerti(CertificationModel certificationModel)
+        public void EditCertificationTest(CertificationModel certificationModel)
         {
             PerformEducationAction(
                 action: () => profileCertObj.EditCert(_driver,
@@ -43,20 +43,55 @@ namespace Test_Project_Mars_Selenium.Tests
             );
         }
 
-        private void PerformEducationAction(Action action, CertificationModel certificationModel, string actionName)
+        [Test]
+        [TestCaseSource(nameof(AddInvalidCert))]
+        public void AddInvalidCertificationTest(CertificationModel certificationModel)
+        {
+            PerformEducationAction(
+                action: () => profileCertObj.AddCert(_driver,
+                                                certificationModel.CertName,
+                                                certificationModel.From,
+                                                certificationModel.AchieveYear),
+                certificationModel: certificationModel,
+                actionName: "Add",
+                assertType: "invalid"
+            );
+        }
+
+        private void PerformEducationAction(Action action, CertificationModel certificationModel, string actionName, string assertType = "default")
         {
             // Act
             action();
 
             // Assert
-            Assert.That(profileCertObj.IsNotiDisplayed(_driver), Is.True, $"Failed to display notification on {actionName}!");
-            Assert.That(profileCertObj.GetDisplayedTxt(_driver, "award Display").Equals(certificationModel.CertName), $"Failed to {actionName} award name!");
-            Assert.That(profileCertObj.GetDisplayedTxt(_driver, "from Display").Equals(certificationModel.From), $"Failed to {actionName} award from!");
-            Assert.That(profileCertObj.GetDisplayedTxt(_driver, "year Display").Equals(certificationModel.AchieveYear), $"Failed to {actionName} year!");
+            switch (assertType)
+            {
+                case "default":
+                    Assert.That(profileCertObj.IsNotiDisplayed(_driver), Is.True, $"Failed to display notification on {actionName}!");
+                    Assert.That(profileCertObj.GetDisplayedTxt(_driver, "award Display").Equals(certificationModel.CertName), $"Failed to {actionName} award name!");
+                    Assert.That(profileCertObj.GetDisplayedTxt(_driver, "from Display").Equals(certificationModel.From), $"Failed to {actionName} award from!");
+                    Assert.That(profileCertObj.GetDisplayedTxt(_driver, "year Display").Equals(certificationModel.AchieveYear), $"Failed to {actionName} year!");
+                    break;
+
+                case "invalid":
+                    if(profileCertObj.GetDisplayedTxt(_driver, "award Display") == certificationModel.CertName && profileCertObj.GetDisplayedTxt(_driver, "from Display") == certificationModel.From)
+                    {
+                        Assert.Fail($"Fail to validate {actionName}");
+                    }
+                    //Assert.Pass();
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+
         }
 
         public static IEnumerable<CertificationModel> AddCert()
             => LoadCertData("ExampleCertification1.json");
+
+        public static IEnumerable<CertificationModel> AddInvalidCert()
+            => LoadCertData("ExampleInvalidCertification.json");
 
         public static IEnumerable<CertificationModel> EditCert()
             => LoadCertData("ExampleCertification2.json");
--- Test_Project_Mars_Selenium/Tests/ProfileEducation.cs
+++ Test_Project_Mars_Selenium/Tests/ProfileEducation.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework.Legacy;
+using OpenQA.Selenium.DevTools.V128.Audits;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,21 +52,61 @@ namespace Test_Project_Mars_Selenium.Tests
 
 
         }
-        
-        private void PerformEducationAction(Action action, EducationModel educationModel, string actionName)
+
+        [Test]
+        [TestCaseSource(nameof(AddInvalidEducation))]
+        public void AddInvalidEducationTest(EducationModel educationModel)
+        {
+            PerformEducationAction(
+
+                action: () => profileEduObj.AddEducation(_driver,
+                                                    educationModel.UniName,
+                                                    educationModel.UniCountry,
+                                                    educationModel.Title,
+                                                    educationModel.Degree,
+                                                    educationModel.GraduationYear),
+                educationModel: educationModel,
+                actionName: "Add",
+                assertType: "invalid"
+            );
+            //Fail as allows to add invalid characters
+        }
+
+        private void PerformEducationAction(Action action, EducationModel educationModel, string actionName, string assertType = "default")
         {
             // Act
             action();
 
             // Assert
-            Assert.That(profileEduObj.IsNotiDisplayed(_driver), Is.True, $"Failed to display notification on {actionName}!");
-            Assert.That(profileEduObj.GetDisplayedTxt(_driver, "uni Display").Equals(educationModel.UniName), $"Failed to {actionName} university name!");
-            Assert.That(profileEduObj.GetDisplayedTxt(_driver, "degree Display").Equals(educationModel.Degree), $"Failed to {actionName} degree!");
-            Assert.That(profileEduObj.GetDisplayedTxt(_driver, "title Display").Equals(educationModel.Title), $"Failed to {actionName} title!");
+            switch (assertType)
+            {   
+                case "default":
+                    Assert.That(profileEduObj.IsNotiDisplayed(_driver), Is.True, $"Failed to display notification on {actionName}!");
+                    Assert.That(profileEduObj.GetDisplayedTxt(_driver, "uni Display").Equals(educationModel.UniName), $"Failed to {actionName} university name!");
+                    Assert.That(profileEduObj.GetDisplayedTxt(_driver, "degree Display").Equals(educationModel.Degree), $"Failed to {actionName} degree!");
+                    Assert.That(profileEduObj.GetDisplayedTxt(_driver, "title Display").Equals(educationModel.Title), $"Failed to {actionName} title!");
+                    break;
+
+                case "invalid":
+
+                    if (profileEduObj.GetDisplayedTxt(_driver, "uni Display") == educationModel.UniName)
+                    {
+                        Assert.Fail($"Fail to validate {actionName} education");
+                    }
+                    //Assert.Pass();
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+
+            }
         }
 
         public static IEnumerable<EducationModel> AddEducation()
             => LoadEducationData("ExampleEducation1.json");
+
+        public static IEnumerable<EducationModel> AddInvalidEducation()
+            => LoadEducationData("ExampleInvalidEducation.json");
 
         public static IEnumerable<EducationModel> EditEducation()
             => LoadEducationData("ExampleEducation2.json");
